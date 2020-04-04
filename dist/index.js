ae9d562f6c8546827431224b73ef8342482e7180
Aleksey Stepanov <stepanovl87@gmail.com>
2020-04-04 09:19:00 +0300

    Сделана адаптивность

--- dist/index.js
+++ dist/index.js
@@ -298,6 +298,17 @@ eval("// extracted by mini-css-extract-plugin\n    if(false) { var cssReload; }\
 
 /***/ }),
 
+/***/ "../node_modules/mini-css-extract-plugin/dist/loader.js?!../node_modules/css-loader/dist/cjs.js!../node_modules/postcss-loader/src/index.js?!../node_modules/sass-loader/dist/cjs.js!./sass/media.scss":
+/*!**********************************************************************************************************************************************************************************************************************!*\
+  !*** ../node_modules/mini-css-extract-plugin/dist/loader.js??ref--5-1!../node_modules/css-loader/dist/cjs.js!../node_modules/postcss-loader/src??ref--5-3!../node_modules/sass-loader/dist/cjs.js!./sass/media.scss ***!
+  \**********************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n    if(false) { var cssReload; }\n  \n\n//# sourceURL=webpack:///./sass/media.scss?../node_modules/mini-css-extract-plugin/dist/loader.js??ref--5-1!../node_modules/css-loader/dist/cjs.js!../node_modules/postcss-loader/src??ref--5-3!../node_modules/sass-loader/dist/cjs.js");
+
+/***/ }),
+
 /***/ "./blocks/about/about.scss":
 /*!*********************************!*\
   !*** ./blocks/about/about.scss ***!
@@ -437,7 +448,7 @@ eval("var api = __webpack_require__(/*! ../../../node_modules/style-loader/dist/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("__webpack_require__(/*! ../../sass/index.scss */ \"./sass/index.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour.scss */ \"./blocks/tour/tour.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour-toskana/tour-toskana.scss */ \"./blocks/tour/tour-toskana/tour-toskana.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour-island/tour-island.scss */ \"./blocks/tour/tour-island/tour-island.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour-norway/tour-norway.scss */ \"./blocks/tour/tour-norway/tour-norway.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour-turkey/tour-turkey.scss */ \"./blocks/tour/tour-turkey/tour-turkey.scss\")\n__webpack_require__(/*! ../../blocks/form/form.scss */ \"./blocks/form/form.scss\")\n__webpack_require__(/*! ../../blocks/user/user.scss */ \"./blocks/user/user.scss\")\n__webpack_require__(/*! ../../blocks/travel/travel.scss */ \"./blocks/travel/travel.scss\")\n__webpack_require__(/*! ../../blocks/communication/comminication.scss */ \"./blocks/communication/comminication.scss\")\n__webpack_require__(/*! ../../blocks/about/about.scss */ \"./blocks/about/about.scss\")\n__webpack_require__(/*! ../../blocks/data/data.scss */ \"./blocks/data/data.scss\")\n__webpack_require__(/*! ../../blocks/submit/submit.scss */ \"./blocks/submit/submit.scss\")\n\n\n//# sourceURL=webpack:///./js/entry/index.js?");
+eval("__webpack_require__(/*! ../../sass/index.scss */ \"./sass/index.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour.scss */ \"./blocks/tour/tour.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour-toskana/tour-toskana.scss */ \"./blocks/tour/tour-toskana/tour-toskana.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour-island/tour-island.scss */ \"./blocks/tour/tour-island/tour-island.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour-norway/tour-norway.scss */ \"./blocks/tour/tour-norway/tour-norway.scss\")\n__webpack_require__(/*! ../../blocks/tour/tour-turkey/tour-turkey.scss */ \"./blocks/tour/tour-turkey/tour-turkey.scss\")\n__webpack_require__(/*! ../../blocks/form/form.scss */ \"./blocks/form/form.scss\")\n__webpack_require__(/*! ../../blocks/user/user.scss */ \"./blocks/user/user.scss\")\n__webpack_require__(/*! ../../blocks/travel/travel.scss */ \"./blocks/travel/travel.scss\")\n__webpack_require__(/*! ../../blocks/communication/comminication.scss */ \"./blocks/communication/comminication.scss\")\n__webpack_require__(/*! ../../blocks/about/about.scss */ \"./blocks/about/about.scss\")\n__webpack_require__(/*! ../../blocks/data/data.scss */ \"./blocks/data/data.scss\")\n__webpack_require__(/*! ../../blocks/submit/submit.scss */ \"./blocks/submit/submit.scss\")\n__webpack_require__(/*! ../../sass/media.scss */ \"./sass/media.scss\")\n\n\n//# sourceURL=webpack:///./js/entry/index.js?");
 
 /***/ }),
 
@@ -449,6 +460,17 @@ eval("__webpack_require__(/*! ../../sass/index.scss */ \"./sass/index.scss\")\n_
 /***/ (function(module, exports, __webpack_require__) {
 
 eval("var api = __webpack_require__(/*! ../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ \"../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\");\n            var content = __webpack_require__(/*! !../../node_modules/mini-css-extract-plugin/dist/loader.js??ref--5-1!../../node_modules/css-loader/dist/cjs.js!../../node_modules/postcss-loader/src??ref--5-3!../../node_modules/sass-loader/dist/cjs.js!./index.scss */ \"../node_modules/mini-css-extract-plugin/dist/loader.js?!../node_modules/css-loader/dist/cjs.js!../node_modules/postcss-loader/src/index.js?!../node_modules/sass-loader/dist/cjs.js!./sass/index.scss\");\n\n            content = content.__esModule ? content.default : content;\n\n            if (typeof content === 'string') {\n              content = [[module.i, content, '']];\n            }\n\nvar options = {};\n\noptions.insert = \"head\";\noptions.singleton = false;\n\nvar update = api(content, options);\n\nvar exported = content.locals ? content.locals : {};\n\n\n\nmodule.exports = exported;\n\n//# sourceURL=webpack:///./sass/index.scss?");
+
+/***/ }),
+
+/***/ "./sass/media.scss":
+/*!*************************!*\
+  !*** ./sass/media.scss ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("var api = __webpack_require__(/*! ../../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ \"../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js\");\n            var content = __webpack_require__(/*! !../../node_modules/mini-css-extract-plugin/dist/loader.js??ref--5-1!../../node_modules/css-loader/dist/cjs.js!../../node_modules/postcss-loader/src??ref--5-3!../../node_modules/sass-loader/dist/cjs.js!./media.scss */ \"../node_modules/mini-css-extract-plugin/dist/loader.js?!../node_modules/css-loader/dist/cjs.js!../node_modules/postcss-loader/src/index.js?!../node_modules/sass-loader/dist/cjs.js!./sass/media.scss\");\n\n            content = content.__esModule ? content.default : content;\n\n            if (typeof content === 'string') {\n              content = [[module.i, content, '']];\n            }\n\nvar options = {};\n\noptions.insert = \"head\";\noptions.singleton = false;\n\nvar update = api(content, options);\n\nvar exported = content.locals ? content.locals : {};\n\n\n\nmodule.exports = exported;\n\n//# sourceURL=webpack:///./sass/media.scss?");
 
 /***/ })
 
