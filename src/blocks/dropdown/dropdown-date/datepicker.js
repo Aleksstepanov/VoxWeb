a6bad31caf20becc5809fcb272091a01af31e544
Aleksey Stepanov <stepanovl87@gmail.com>
2020-04-07 20:33:29 +0300

    readme.md

--- src/blocks/dropdown/dropdown-date/datepicker.js
+++ src/blocks/dropdown/dropdown-date/datepicker.js
@@ -1,18 +1,18 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    let elems = document.querySelectorAll('.datepicker');
-    let options = {
-      i18n: {
-        months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-        weekdaysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-        weekdaysAbbrev: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
-      },
-      autoClose: true,
-      defaultDate: new Date(),
-      setDefaultDate: true,
-      disableWeekends: false,
-      firstDay: 1,
-      
-    }
-    let instances = M.Datepicker.init(elems, options);
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  let elems = document.querySelectorAll('.datepicker');
+  let options = {
+    i18n: {
+      months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+      weekdaysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+      weekdaysAbbrev: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+    },
+    autoClose: true,
+    defaultDate: new Date(),
+    setDefaultDate: true,
+    disableWeekends: false,
+    firstDay: 1,
+    
+  }
+  let instances = M.Datepicker.init(elems, options);
+});
 
